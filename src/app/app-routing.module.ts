bb909de8045186e2cb67295d30425a52ca9d1fca
Luchopaulucci <luchopaluch@gmail.com>
2022-12-11 21:18:10 -0300

    componentes y ruteo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { IndexComponent } from './componentes/index/index.component';
+import { LoginComponent } from './componentes/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: IndexComponent,},
+  {path: 'Login', component: LoginComponent},
+  {path: 'Acercade', component:AcercaDeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './componentes/nav-bar/nav-bar.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { SkillsComponent } from './componentes/skills/skills.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { IndexComponent } from './componentes/index/index.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    AcercaDeComponent,
+    EducacionComponent,
+    SkillsComponent,
+    ExperienciaComponent,
+    LoginComponent,
+    InicioComponent,
+    ProyectosComponent,
+    FooterComponent,
+    IndexComponent,
   ],
   imports: [
     BrowserModule,
